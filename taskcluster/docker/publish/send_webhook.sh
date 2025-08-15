2bcd9b33ed46b19b4e01c2b82bd8da918de11eaa
Bastien Orivel <eijebong@bananium.fr>
2025-08-15 14:39:53 +0200

    Remove kubectl stuff from webhook
    This hasn't been necessary for a while now and is failing on main

--- taskcluster/docker/publish/send_webhook.sh
+++ taskcluster/docker/publish/send_webhook.sh
@@ -3,15 +3,9 @@
 SECRET_URL="http://taskcluster/secrets/v1/secret/ap-lobby"
 SECRET=$(curl ${SECRET_URL})
 
-KUBECFG=$(echo "${SECRET}" | jq -r '.secret.kubecfg')
-mkdir -p ${HOME}/.kube
-echo "${KUBECFG}" > ${HOME}/.kube/config
-
 KEY=$(echo "${SECRET}" | jq -r '.secret.admin_key_staging')
 curl -H "X-Api-Key: ${KEY}" https://ap-lobby-stg.bananium.fr/worlds/refresh
-kubectl rollout -n ap-lobby-stg restart deployment yaml-checker-stg
 
 KEY=$(echo "${SECRET}" | jq -r '.secret.admin_key_prod')
 curl -H "X-Api-Key: ${KEY}" https://ap-lobby.bananium.fr/worlds/refresh
-kubectl rollout -n ap-lobby-prod restart deployment yaml-checker-prod
 
